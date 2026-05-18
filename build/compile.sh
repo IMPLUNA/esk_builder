f8f33af4f2b78689d79eaeb9fd15dea1afc03ead
IMPLUNA <90521766+IMPLUNA@users.noreply.github.com>
2026-05-18 09:24:39 +0000

    fix(build): patch CONFIG_LSM in .config after make defconfig for BBG

--- build/compile.sh
+++ build/compile.sh
@@ -12,25 +12,31 @@ build_kernel() {
 
     prune_bad_artifacts "$KERNEL_OUT"
 
+    # BBG: update defconfig BEFORE merge so it propagates
+    if is_true "$BBG"; then
+        local defconfig_file="$KERNEL/arch/arm64/configs/$KERNEL_DEFCONFIG"
+        if grep -q 'CONFIG_LSM=' "$defconfig_file"; then
+            sed -i 's/CONFIG_LSM=\(.*\)/CONFIG_LSM=\1,baseband_guard/' "$defconfig_file"
+        else
+            echo 'CONFIG_LSM="selinux,baseband_guard"' >> "$defconfig_file"
+        fi
+    fi
+
     if [[ "$BUILD_TARGET" == xaga ]]; then
         info "Merging defconfig"
         local configs="arch/arm64/configs"
         KCONFIG_CONFIG="$configs/gki_defconfig" scripts/kconfig/merge_config.sh -m -r "$configs/gki_defconfig" "$configs/vendor/xiaomi_mt6895.config" "$configs/vendor/xaga.config"
     fi
 
-    # BBG: add baseband_guard to CONFIG_LSM after defconfig merge
-    if is_true "$BBG"; then
-        local defconfig_file="$KERNEL/arch/arm64/configs/$KERNEL_DEFCONFIG"
-        if grep -q 'CONFIG_LSM=' "$defconfig_file"; then
-            sed -i 's/CONFIG_LSM=\(.*\)/CONFIG_LSM=\1,baseband_guard/' "$defconfig_file"
-        else
-            echo 'CONFIG_LSM=selinux,baseband_guard' >> "$defconfig_file"
-        fi
-        info "CONFIG_LSM updated for BBG"
-    fi
-
     info "Generate defconfig: $KERNEL_DEFCONFIG"
     make "${MAKE_ARGS[@]}" "$KERNEL_DEFCONFIG"
+
+    # BBG: fix CONFIG_LSM in generated .config (make defconfig overwrites it)
+    if is_true "$BBG"; then
+        sed -i 's/CONFIG_LSM=\(.*\)/CONFIG_LSM=\1,baseband_guard/' "$KERNEL_OUT/.config"
+        make "${MAKE_ARGS[@]}" olddefconfig
+        info "CONFIG_LSM patched in .config for BBG"
+    fi
 
     info "Building Image and modules..."
     make "${MAKE_ARGS[@]}" Image modules
